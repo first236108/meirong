fb8e17cb5c4fe4f068810d0183cdd47c6faf5fea
潘志强 <85210755@qq.com>
2018-05-30 23:37:45 +0800

    complete the login/index.html

--- application/admin/controller/Login.php
+++ application/admin/controller/Login.php
@@ -9,16 +9,33 @@
 namespace app\admin\controller;
 
 use think\Controller;
+use think\captcha\Captcha;
 
 Class Login extends Controller
 {
     public function index()
     {
         if (request()->isPost()) {
-            //todo
+            dump(input('param.'));
         }
         $config = getSiteConfig('site_info');
         $this->assign('config', $config);
         return view();
+    }
+
+    public function verify()
+    {
+        $code    = input('code', 'login');
+        $captcha = new Captcha();
+        return $captcha->entry($code);
+    }
+
+    public function check($value, $code)
+    {
+        $captcha = new Captcha();
+        if (!$captcha->check($value, $code)) {
+            return false;
+        }
+        return true;
     }
 }
