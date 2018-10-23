f214a561650e179d038c1f9b4a7ce6e016abaa6c
alex@ThinkPad <85210755@qq.com>
2018-10-23 23:38:27 +0800

    select the echarts.js and upgrade the thinkphp framework version to 5.1.27

--- application/admin/controller/Report.php
+++ application/admin/controller/Report.php
@@ -5,15 +5,30 @@
  * NickName: 柏宇娜
  * Date: 2018/6/10 0:25
  */
+
 namespace app\admin\controller;
+
 use think\Db;
 
 class Report extends Base
 {
-    public function imcome()
+    public function income()
     {
-
-        return view();
+        if ($this->request->isGet())
+            return view();
+        $str        = '-7 day';
+        $start_time = strtotime(date('Y-m-d', strtotime($str)));
+        $end_time   = time();
+        $map        = [
+            ['status', 'IN', [1, 5]],
+            ['pay_status', '=', 1],
+            ['type', '=', 0],
+            ['pay_amount', '>', 0],
+            ['pay_time', '>', $start_time]
+        ];
+        $data       = Db::name('order')->fetchSql(true)->where($map)->field('SUM(pay_amount),pay_time,confirm_id')->group('confirm_id')->select();
+        dump($data);
+        die;
     }
 
     public function service()
