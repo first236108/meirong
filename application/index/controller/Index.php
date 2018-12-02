65d2040fa4cae062e4870a54007ee57b1695359d
alex@ThinkPad <85210755@qq.com>
2018-12-02 19:54:35 +0800

    start develop the front page

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,17 +1,38 @@
 <?php
+
 namespace app\index\controller;
+
+use think\Controller;
 use think\Db;
-class Index
+
+class Index extends Controller
 {
     public function index()
     {
-        if (request()->isGet())
+        if (request()->isGet()) {
+            $recommend = Db::name('item')->where("is_recommend=1 AND is_delete=0 AND on_sale=1")->order('sort desc')->select();
+            $this->assign('recommend', $recommend);
             return view();
+        }
 
-        $result=Db::name('item')->select();
+        $page   = input('page/d', 1);
+        $size   = input('pagesize/d', 2);
+        $result = Db::name('item')->where("is_delete=0 AND on_sale=1")->limit($size)->page($page)->select();
         if (!$result)
-            return json('错误',404);
+            return json('错误', 404);
 
         return json($result);
+    }
+
+    public function detail()
+    {
+        $id  = input('id', 0);
+        $row = Db::name('item')->where("item_id={$id} AND on_sale=1 AND is_delete=0")->find();
+        if (!$row)
+            $this->error('您要查看的页面没找到哦!~');
+
+        $row['img'] = Db::name('item_img')->where('item_id', $id)->order('sort desc')->select();
+        $this->assign('row', $row);
+        return $this->fetch();
     }
 }
